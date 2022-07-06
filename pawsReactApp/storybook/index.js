92211536a8ae70e0702a56b2bb6882af9c69abad
NicoleSelig <nlselig@sep.com>
2022-07-06 19:09:26 -0400

    successfully added reloader

--- pawsReactApp/storybook/index.js
+++ pawsReactApp/storybook/index.js
@@ -1,9 +1,10 @@
-// if you use expo remove this line
-import { AppRegistry } from 'react-native';
+/* eslint-disable no-undef */
 
+// if you use expo remove this line
+import { AppRegistry, Platform} from 'react-native';
 import { getStorybookUI, configure, addDecorator } from '@storybook/react-native';
 import { withKnobs } from '@storybook/addon-knobs';
-
+import { loadStories } from './storyLoader';
 import './rn-addons';
 
 // enables knobs for all stories
@@ -11,12 +12,15 @@ addDecorator(withKnobs);
 
 // import stories
 configure(() => {
-  require('./stories');
+  loadStories();
 }, module);
 
 // Refer to https://github.com/storybookjs/react-native/tree/master/app/react-native#getstorybookui-options
 // To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({});
+const StorybookUIRoot = getStorybookUI({
+  host: Platform.OS === 'android' ? '10.0.2.2' : '0.0.0.0',
+  asyncStorage: require('@react-native-community/async-storage').default
+});
 
 // If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
 // If you use Expo you should remove this line.
