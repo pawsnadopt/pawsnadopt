e0cb0bd3f9664c35df2ecba65c20d6db2b9a027d
Nicole Selig <nicoleleeselig@gmail.com>
2022-07-10 16:53:34 -0400

    add terniary operator

--- pawsReactApp/App.tsx
+++ pawsReactApp/App.tsx
@@ -3,7 +3,7 @@ import {NativeFixtureLoader} from 'react-cosmos/native'
 // You generate cosmos.userdeps.js when you start the Cosmos server
 import {rendererConfig, fixtures, decorators} from './cosmos.userdeps.js'
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
       <NativeFixtureLoader
@@ -15,3 +15,4 @@ export default class App extends Component {
   }
 }
 
+export default Config.LOAD_COSMOS === 'true' ? StorybookUI : App
